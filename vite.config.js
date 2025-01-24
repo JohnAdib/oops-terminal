3fa2e7dd7a7f4ae573ff055fb6c65d70219b45ee
John Adib <mr.john.adib@gmail.com>
2025-01-24 19:57:12 +0000

    Refactor Vite configuration: Update entry point in vite.config.js to use path.resolve for better compatibility, and import path and fileURLToPath for improved directory handling.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), cssInjectedByJsPlugin()],
   build: {
     lib: {
-      entry: './src/index.ts',
+      entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'OopsTerminal',
       fileName: (format) => `oops-terminal.${format}.js`,
       formats: ['es', 'cjs', 'umd'],
